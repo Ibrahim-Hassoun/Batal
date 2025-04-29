7c64e969d7a3b0a60d3b6492a400567c2b202a59
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-04-29 22:08:10 +0300

    added register request

---
+++ backend/app/Http/Requests/RootRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+class RootRequest extends FormRequest
+{
+    use \App\Http\Traits\HttpResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            respond(false,$validator->errors,null,422) 
+        );
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
