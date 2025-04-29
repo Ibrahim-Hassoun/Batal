d1a4fb4c0e3a5cd62674504d7fb296554838836a
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-04-29 15:00:46 +0300

    added auth controller

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+       
+    }
+
+    public function login(Request $request)
+    {
+        // Login logic here
+       
+    }
+
+    public function logout(Request $request)
+    {
+        // Logout logic here
+       
+    }
+}
