41bac634ed698e86b15f7d6d67ab040d07bd38ea
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-04-30 20:13:20 +0300

    implemented gym factory and seeder,also fixed migration problems

--- backend/database/factories/GymFactory.php
+++ backend/database/factories/GymFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Gym>
@@ -17,7 +18,35 @@ class GymFactory extends Factory
     public function definition(): array
     {
         return [
-            //
-        ];
-    }
+            
+            'owner_id' => \App\Models\User::factory()->create()->id,
+            'name' => $this->faker->company,
+            'description' => $this->faker->paragraph,
+            'logo_path' => $this->faker->imageUrl(),
+            'estimated_capacity' => $this->faker->numberBetween(50, 500),
+            'subscription_price' => $this->faker->numberBetween(100, 1000),
+            'current_traffic' => $this->faker->randomElement(['low', 'medium', 'high']),
+            'usual_traffic' => $this->faker->randomElement(['low', 'medium', 'high']),
+            'opening_hours' => json_encode([
+                'Monday' => ['09:00', '21:00'],
+                'Tuesday' => ['09:00', '21:00'],
+                'Wednesday' => ['09:00', '21:00'],
+                'Thursday' => ['09:00', '21:00'],
+                'Friday' => ['09:00', '21:00'],
+                'Saturday' => ['10:00', '18:00'],
+                'Sunday' => ['10:00', '18:00'],
+        ]),
+            'gender' => $this->faker->randomElement(['male', 'female', 'mixed']),
+            'longitude' => $this->faker->longitude,
+            'latitude' => $this->faker->latitude,
+            'phone_number' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->safeEmail,
+            'website' => $this->faker->url,
+            'is_active' => $this->faker->boolean,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'deleted_at' => null,
+
+    ];
+}
 }
--- backend/database/migrations/2025_04_30_155652_create_gyms_table.php
+++ backend/database/migrations/2025_04_30_155652_create_gyms_table.php
@@ -13,17 +13,18 @@ return new class extends Migration
     {
         Schema::create('gyms', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
             $table->string('description');
             $table->string('logo_path')->nullable();
             $table->integer('estimated_capacity');
             $table->integer('subscription_price');
-            $table->integer('current_traffic')->nullable();
+            $table->enum('current_traffic', ['low', 'medium', 'high'])->nullable();
             $table->enum('usual_traffic', ['low', 'medium', 'high'])->nullable();
             $table->json('opening_hours')->nullable();
             $table->enum('gender', ['male', 'female','mixed']);
-            $table->point('location')->nullable();
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
             $table->string('phone_number');
             $table->string('email');
             $table->string('website')->nullable();
@@ -33,7 +34,8 @@ return new class extends Migration
 
 
             $table->index('name');  
-            $table->spatialIndex('location');
+            $table->index(['latitude', 'longitude']);
+
         });
     }
 
