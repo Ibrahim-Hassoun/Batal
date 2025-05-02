c416fbe0ec09536ad8a211ccb76256e336a22322
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-02 15:50:28 +0300

    implemented posts migration

--- backend/database/migrations/2025_05_02_122856_create_posts_table.php
+++ backend/database/migrations/2025_05_02_122856_create_posts_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->unsignedInteger('reactions')->default(0);
+            $table->unsignedInteger('comments')->default(0);
+            $table->unsignedInteger('shares')->default(0);
+            $table->unsignedInteger('impressions_count')->default(0);
+            $table->unsignedInteger('followers_brought')->default(0);
+            $table->unsignedInteger('profile_visits')->default(0);
+            $table->unsignedInteger('reach')->default(0);
+            $table->timestamp('scheduled_at')->nullable();
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->index('user_id');
+            $table->index(['created_at', 'reactions']);
+
         });
     }
 
