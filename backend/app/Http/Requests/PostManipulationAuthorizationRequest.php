a6321ca3aef7a3a85900f85d73402cf3ef6a8165
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-02 17:43:02 +0300

    added post authorization request

---
+++ backend/app/Http/Requests/PostManipulationAuthorizationRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Post;
+use App\Http\Requests\RootRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostManipulationAuthorizationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $userId = $this->user()->id;
+        $postId = $this->input('id');
+        $post = Post::find($postId);
+        if ($post && $post->user_id == $userId) {
+            return true;
+        }
+        return false;
+        
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
