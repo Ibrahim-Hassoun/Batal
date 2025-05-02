4fff66e4cc4a9c0aa58772ec1fa131213e1bed16
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-02 15:56:11 +0300

    implemented posts factory and seeder

--- backend/database/factories/PostFactory.php
+++ backend/database/factories/PostFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,20 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory()->create()->id,
+            'description' => $this->faker->optional()->sentence(),
+            'image_path' => $this->faker->optional()->imageUrl(),
+            'reactions' => $this->faker->numberBetween(0, 1000),
+            'comments' => $this->faker->numberBetween(0, 500),
+            'shares' => $this->faker->numberBetween(0, 300),
+            'impressions_count' => $this->faker->numberBetween(0, 10000),
+            'followers_brought' => $this->faker->numberBetween(0, 200),
+            'profile_visits' => $this->faker->numberBetween(0, 1000),
+            'reach' => $this->faker->numberBetween(0, 15000),
+            'scheduled_at' => $this->faker->optional()->dateTimeBetween('-1 week', '+1 week'),
+            'deleted_at' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\GymSeeder;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\PostSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             LogSeeder::class,
             GymSeeder::class,
+            PostSeeder::class,
         ]);
         // User::factory(10)->create();
 
