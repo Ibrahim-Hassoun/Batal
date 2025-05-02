16f4a0b9bfcaf3d29a37969fcfe3f416216097de
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-02 16:02:01 +0300

    prepared post controller,services and services provider

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\PostServices\PostServices;
 
 class PostController extends Controller
 {
-    //
+    protected $postServices;
+
+    public function _construct(PostServices $postServices)
+    {
+        $this->postServices = $postServices;
+    }
+
+    public function addPost(Request $request)
+    {
+        
+    }
+    public function getPost(Request $request)
+    {
+        
+    }
+    public function deletePost(Request $request)
+    {
+        
+    }
+    public function updatePost(Request $request)
+    {
+        
+    }
+    
 }
---
+++ backend/app/Services/PostServices/PostServices.php
@@ -0,0 +1,24 @@
+<?php
+
+class Post {
+
+    public function addPost($data)
+    {
+
+    }
+    public function getPost($data)
+    {
+
+    }
+    public function deletePost($data)
+    {
+
+    }
+    public function updatePost($data)
+    {
+
+    }
+    
+
+
+}
