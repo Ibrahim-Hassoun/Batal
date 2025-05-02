279542870c7608dc8ce7f27cc52a1b9c51d3c767
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-02 19:49:36 +0300

    added testSearchPost

--- backend/tests/Feature/PostTest.php
+++ backend/tests/Feature/PostTest.php
@@ -32,7 +32,7 @@ class PostTest extends TestCase
     {
         $data = [
             
-            "description"=>"",
+            "description"=>"test",
             "image_path"=> "https://example.com/images/post123.jpg",
             "scheduled_at"=> "2025-05-03T15:30:00",
         ];
@@ -51,5 +51,16 @@ class PostTest extends TestCase
         $response = $this->addPost($token);
 
         $response->assertStatus(201);
+    }
+
+    public function testSearchPost():void
+    {
+        $token = $this->addUserAndGetToken();
+        $response1 = $this->addPost($token);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+                        ->get('/api/v0.1/posts/search?description=test');
+
+        $response->assertStatus(200);
     }
 }
