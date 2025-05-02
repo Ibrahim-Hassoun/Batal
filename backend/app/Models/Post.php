0b843e779d108dc54fce0136377b97632ef1bed1
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-02 17:39:11 +0300

    added model relations between post and user

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Post extends Model
 {
@@ -24,4 +26,9 @@ class Post extends Model
             'scheduled_at' ,
             'deleted_at' 
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
