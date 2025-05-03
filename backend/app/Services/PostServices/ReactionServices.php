3209487fcca55f6dbe60bfb8fd607b4ca57c454e
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-03 17:40:35 +0300

    implemented dynamic location by ip

--- backend/app/Services/PostServices/ReactionServices.php
+++ backend/app/Services/PostServices/ReactionServices.php
@@ -3,18 +3,27 @@
 namespace App\Services\PostServices;
 
 use App\Models\Reaction;
+use Illuminate\Support\Facades\Http;
+
 
 class ReactionServices
 {
+    
+
     public function addReaction($payload)
     {
+        $ip = request()->ip(); // or use a specific IP
+
+        $response = Http::get("https://ipapi.co/178.135.15.202/json/");
+        $location = $response->json();
+
         $reaction=Reaction::create([
             'user_id' => auth()->user()->id,
             'post_id' => $payload['post_id']??null,
             'type'=> $payload['type']??null,
-            'country'=> $payload['country']??null,
-            'province'=> $payload['province']??null,
-            'city'=> $payload['city']??null,
+            'country'=> $location['country']??null,
+            'province'=> $location['region']??null,
+            'city'=> $location['city']??null,
         ]);
         if(!$reaction){
             throw new Exception("couldn't add reaction",400);
