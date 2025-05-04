059ebef709f94aa21f36697b37eb2afa5b9cc920
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-04 13:36:08 +0300

    implemented addJobProfile validation

--- backend/app/Http/Controllers/JobProfileController.php
+++ backend/app/Http/Controllers/JobProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponseTrait;
 use App\Services\JobProfileServices\JobProfileServices;
+use App\Http\Requests\JobProfileRequests\AddJobProfileRequest;
 
 class JobProfileController extends Controller
 {
@@ -16,7 +17,7 @@ class JobProfileController extends Controller
         $this->jobProfileServices = $jobProfileServices;
     }
 
-    public function addJobProfile(Request $request)
+    public function addJobProfile(AddJobProfileRequest $request)
     {
         try{
             $jobProfile = $this->jobProfileServices->addJobProfile($request);
