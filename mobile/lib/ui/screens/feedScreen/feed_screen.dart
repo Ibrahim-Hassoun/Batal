acbbe95de2650dd48e4c12ac13fb17ecc9478148
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-06 20:23:17 +0300

    finished feed app bar

--- mobile/lib/ui/screens/feedScreen/feed_screen.dart
+++ mobile/lib/ui/screens/feedScreen/feed_screen.dart
@@ -5,6 +5,7 @@ class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: FeedAppBar(),
       body: Center(
         child: Text('Hey, this is feed screen'),
--- mobile/lib/ui/widgets/feed/feed_app_bar.dart
+++ mobile/lib/ui/widgets/feed/feed_app_bar.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/main.dart';
 
 class FeedAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      backgroundColor: Colors.white,
       title: Row(
         mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        
         children: [
-          Icon(Icons.add, color: Colors.black, size: 24),
-          Icon(Icons.search, color: Colors.black, size: 24),
-          Icon(Icons.chat, color: Colors.black, size: 24),
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: secondaryColor, width: 2),
+            ),
+            child: Icon(Icons.add, color: secondaryColor, size:24),
+            ),
+            SizedBox(width: 16),
+          Icon(Icons.search, color: secondaryColor, size:32),
+          SizedBox(width: 16),
+          Icon(Icons.chat, color: secondaryColor, size: 32),
         ],
       ),
     );
