7a454bf1efc292625e8a4a985c044f1499603501
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-07 15:48:41 +0300

    implemented final fix for bottom navbar item

--- mobile/lib/ui/widgets/bottom_navbar/bottom_navbar.dart
+++ mobile/lib/ui/widgets/bottom_navbar/bottom_navbar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mobile/main.dart';
+import './bottom_navbar_item.dart';
 
 class BottomNavbar extends StatefulWidget implements PreferredSizeWidget {
   
@@ -12,21 +15,47 @@ class BottomNavbar extends StatefulWidget implements PreferredSizeWidget {
 class _BottomNavbarState extends State<BottomNavbar>{
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
+    return Container(
+      margin: const EdgeInsets.only(left: 16, right: 16, bottom: 24),
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      height: 56,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 31, 30, 31),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: 
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50,
+              child:   BottomNavbarItem(icon: 'assets/house.svg',label: 'Home',route: '/feed',),
+            ),
+            SizedBox(
+              height: 50,
+              child:   BottomNavbarItem(icon: 'assets/house.svg',label: 'Home',route: '/feed',),
+            ),
+            SizedBox(
+              height: 50,
+              child:   BottomNavbarItem(icon: 'assets/house.svg',label: 'Home',route: '/feed',),
+            ),
+            SizedBox(
+              height: 50,
+              child:   BottomNavbarItem(icon: 'assets/house.svg',label: 'Home',route: '/feed',),
+            ),
+            SizedBox(
+              height: 50,
+              child:   BottomNavbarItem(icon: 'assets/house.svg',label: 'Home',route: '/feed',),
+            ),
+            SizedBox(
+              height: 50,
+              child:   BottomNavbarItem(icon: 'assets/house.svg',label: 'Home',route: '/feed',),
+            ),
+          
+            
+        ],),
+        
     );
   }
 }
--- mobile/lib/ui/widgets/bottom_navbar/bottom_navbar_item.dart
+++ mobile/lib/ui/widgets/bottom_navbar/bottom_navbar_item.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mobile/main.dart';
 
 class BottomNavbarItem extends StatefulWidget implements PreferredSizeWidget {
   
+  final String icon;
+  final String label;
+  final String route;
+
+  BottomNavbarItem({
+    required this.icon,
+    required this.label,
+    required this.route,
+  });
+
   @override
   _BottomNavbarItemState createState() => _BottomNavbarItemState();
 
@@ -9,11 +21,41 @@ class BottomNavbarItem extends StatefulWidget implements PreferredSizeWidget {
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
 
-class _BottomNavbarItemState extends State<BottomNavbarItem>{
+class _BottomNavbarItemState extends State<BottomNavbarItem> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-        
+    return SizedBox(
+      height: 50,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 20,
+            child: SvgPicture.asset(
+              widget.icon,
+              width: 20,
+              height: 20,
+              color: primaryColor,
+            ),
+          ),
+          const SizedBox(height: 4),
+          SizedBox(
+            height: 6,
+            child: FittedBox( // NEW: scales the text to fit
+              fit: BoxFit.scaleDown,
+              child: Text(
+                widget.label,
+                style: const TextStyle(
+                  fontSize: 12,
+                  height: 0.1, // Tight line height
+                  color: primaryColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
