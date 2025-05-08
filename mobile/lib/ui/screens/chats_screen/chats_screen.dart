5176ac0b721f3e62b69dcd4e168df9b987d2593f
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-08 19:31:04 +0300

    initialized chats screen

---
+++ mobile/lib/ui/screens/chats_screen/chats_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chats'),
+      ),
+      body: Center(
+        child: Text('This is the Chats Screen'),
+      ),
+    );
+  }
+}
