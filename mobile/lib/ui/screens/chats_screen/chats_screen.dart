f8eee3f630cdfdc7df5f89f96cf8199016b00941
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-08 19:41:51 +0300

    added search in cahts

--- mobile/lib/ui/screens/chats_screen/chats_screen.dart
+++ mobile/lib/ui/screens/chats_screen/chats_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/main.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({Key? key}) : super(key: key);
@@ -12,7 +13,17 @@ class _ChatsScreenState extends State<ChatsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chats'),
+        title: TextField(
+          decoration: InputDecoration(
+        hintText: 'Search',
+        fillColor: bg_gray,
+        filled: true,
+        
+          ),
+          style: TextStyle(color: secondaryColor),
+        ),
+        backgroundColor: tertiaryColor,
+      
       ),
       body: Center(
         child: Text('This is the Chats Screen'),
