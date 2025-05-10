38158f3788ed95ad9acc9b7b5ac89c314e11bc29
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-10 16:03:59 +0300

    transformed CameraImage into InputImage

--- mobile/lib/core/camera_logic/camera_logic.dart
+++ mobile/lib/core/camera_logic/camera_logic.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:mobile/core/provider/workout_provider.dart';
@@ -8,6 +9,13 @@ import '../tensorflow/tensorflow.dart';
 class CameraLogic {
 TensorflowFunctions tensorflowFunctions = TensorflowFunctions();
 
+late final camera ;
+final _orientations = {
+  DeviceOrientation.portraitUp: 0,
+  DeviceOrientation.landscapeLeft: 90,
+  DeviceOrientation.portraitDown: 180,
+  DeviceOrientation.landscapeRight: 270,
+};
 
 Future<void> initializeCamera(WorkoutProvider workoutProvider) async {
     
@@ -15,7 +23,7 @@ Future<void> initializeCamera(WorkoutProvider workoutProvider) async {
     
     
     int selectedCameraIdx = 1;
-
+    this.camera = cameras[selectedCameraIdx];
     
     workoutProvider.setController ( CameraController(
       cameras[selectedCameraIdx],
@@ -53,4 +61,26 @@ Future<void> initializeCamera(WorkoutProvider workoutProvider) async {
     workoutProvider.controller!.dispose();
     
   }
+InputImage? _inputImageFromCameraImage(CameraImage image) {
+  final camera =  this.camera;
+  
+   final format = InputImageFormatValue.fromRawValue(image.format.raw);
+
+   if (format == null ||
+          (Platform.isAndroid && format != InputImageFormat.nv21) ||
+          (Platform.isIOS && format != InputImageFormat.bgra8888)) return null;
+
+      if (image.planes.length != 1) return null;
+  final plane = image.planes.first;
+
+    return InputImage.fromBytes(
+    bytes: plane.bytes,
+     metadata: InputImageMetadata(
+      size: Size(image.width.toDouble(), image.height.toDouble()),
+      rotation: InputImageRotation.rotation0deg, // used only in Android
+      format: format, // used only in iOS
+      bytesPerRow: plane.bytesPerRow, // used only in iOS
+    ),
+    );
+} 
 }
