59bfcc8a4af3f393cbb9379bef937e33664d7e61
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-10 21:30:35 +0300

    implemented addExercice controller

--- backend/app/Http/Controllers/ExerciceController.php
+++ backend/app/Http/Controllers/ExerciceController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class ExerciceController extends Controller
 {
-    //
+    public function addExercice(Request $request)
+    {
+        try{
+            $post = $this->exerciceServices->addExercice($request->all());
+            return $this->respond(true,'Exercice added successfully',$post,201);
+
+        }catch(\Exception $e){
+            return $this->respond(false,$e->getMessage(),null,$e->getCode());
+        }
+    }
 }
