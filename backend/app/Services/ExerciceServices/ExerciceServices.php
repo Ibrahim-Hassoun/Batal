34409567fc8f1d8c7c2940080fd43898561c366b
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-10 21:37:33 +0300

    implemented addExercice service

--- backend/app/Services/ExerciceServices/ExerciceServices.php
+++ backend/app/Services/ExerciceServices/ExerciceServices.php
@@ -9,6 +9,23 @@ class ExerciceServices
 {
     public function addExercice($request)
     {
-        
+        $exercice = Exercice::create([
+            'area' => $request->area,
+            'muscle' => $request->muscle,
+            'exercice' => $request->exercice,
+            'description' => $request->description ?? null,
+            'difficulty' => $request->difficulty ?? null,
+            'equipment' => $request->equipment ?? null,
+            'type' => $request->type ?? null,
+            'duration' => $request->duration ?? null,
+            'image_url' => $request->image_url ?? null,
+            'video_url' => $request->video_url ?? null,
+            'tags' => $request->tags?? null
+        ]);
+
+        if (!$exercice){
+            throw new \Exception('Error creating exercice', 500);
+        }
+        return $exercice;
     }
 }
