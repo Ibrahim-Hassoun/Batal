63c0530608cd2dbb7148eef2d9460b09382add62
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-11 14:07:54 +0300

    implemented chatbot messages factory

--- backend/database/factories/ChatbotMessageFactory.php
+++ backend/database/factories/ChatbotMessageFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\ChatbotSession;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,12 @@ class ChatbotMessageFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'chatbot_session_id' => ChatbotSession::factory()->create()->id, 
+            'role' => $this->faker->randomElement(['user', 'assistant']), 
+            'content' => $this->faker->text(), 
+            'meta' => $this->faker->optional()->randomElement([null, json_encode(['token_usage' => rand(100, 500)])]),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
