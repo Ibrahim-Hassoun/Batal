79ce597bb33cdaced550d91bd6d321669a51ba19
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-11 12:11:23 +0300

    imported response trait

--- backend/app/Http/Controllers/ChatbotController.php
+++ backend/app/Http/Controllers/ChatbotController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits\HttpResponseTrait;
 
 class ChatbotController extends Controller
 {
+    use HttpResponseTrait;
+    
     public function sendMessage(Request $request)
     {
         
