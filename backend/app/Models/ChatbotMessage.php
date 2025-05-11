8ca5b2b031682c72eefd11e72a33dfb3a0846a84
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-11 15:29:34 +0300

    added chatbot messages' fillables

--- backend/app/Models/ChatbotMessage.php
+++ backend/app/Models/ChatbotMessage.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class ChatbotMessage extends Model
 {
     /** @use HasFactory<\Database\Factories\ChatbotMessageFactory> */
+    use SoftDeletes;
     use HasFactory;
+    
     protected $fillable = [
         'chatbot_session_id',
         'role',
--- backend/app/Services/ChatbotServices/ChatbotServices.php
+++ backend/app/Services/ChatbotServices/ChatbotServices.php
@@ -9,7 +9,7 @@ use Prism\Prism\Enums\Provider;
 class ChatbotServices
 {
     public function sendMessage($request)
-    {
+    {   
         $response = Prism::text()
         ->using(Provider::OpenAI, 'gpt-4')
         ->withSystemPrompt('You are a helpful assistant. Respond only to gym related queries,Make your responses short and concise.')
