b011fe5c634b9bc1166a3fa725264a5caa252c35
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-11 12:17:27 +0300

    injected service into controller constructor

--- backend/app/Http/Controllers/ChatbotController.php
+++ backend/app/Http/Controllers/ChatbotController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Traits\HttpResponseTrait;
+use App\Services\ChatbotServices\ChatbotServices;
 
 class ChatbotController extends Controller
 {
     use HttpResponseTrait;
-    
+    use ChatbotServices;
+
+    public function __construct(ChatbotServices $chatbotServices)
+    {
+        $this->chatbotServices = $chatbotServices;
+    }
+
     public function sendMessage(Request $request)
     {
         
