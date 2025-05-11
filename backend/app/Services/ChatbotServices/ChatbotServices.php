b8548f79875b406e0521335211159bac3623e443
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-11 12:13:28 +0300

    initialized sendMessage service

---
+++ backend/app/Services/ChatbotServices/ChatbotServices.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Services\ChatbotServices\ChatbotServices;
+
+use Prism\Prism\Prism;
+use Prism\Prism\Enums\Provider;
+
+
+class ChatbotServices
+{
+    public function sendMessage($request)
+    {
+
+    }
+}
