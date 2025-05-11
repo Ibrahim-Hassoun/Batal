a8d34eb6d46d715526d266ec75a1fe2148352619
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-11 12:14:41 +0300

    implemented sendMessage service

--- backend/app/Services/ChatbotServices/ChatbotServices.php
+++ backend/app/Services/ChatbotServices/ChatbotServices.php
@@ -10,6 +10,11 @@ class ChatbotServices
 {
     public function sendMessage($request)
     {
+        $response = Prism::text()
+        ->using(Provider::Anthropic, 'claude-3-5-sonnet-20241022')
+        ->withPrompt('Tell me a short story about a brave knight.')
+        ->asText();
 
+        return $response;
     }
 }
