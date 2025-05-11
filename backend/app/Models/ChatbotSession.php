01d4a5675c1118822c9d8a73bb6213742f0ac087
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-11 14:14:28 +0300

    added relations between chatbotMessage and chatbotSession models

--- backend/app/Models/ChatbotSession.php
+++ backend/app/Models/ChatbotSession.php
@@ -2,14 +2,25 @@
 
 namespace App\Models;
 
-
+use App\Models\ChatbotMessage;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class ChatbotSession extends Model
 {
     /** @use HasFactory<\Database\Factories\ChatbotSessionFactory> */
     use HasFactory;
     use softDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'external_session_id',
+    ];
+
+    public function ChatbotMessages()
+    {
+        return $this->hasMany(ChatbotMessage::class);
+    }
 }
