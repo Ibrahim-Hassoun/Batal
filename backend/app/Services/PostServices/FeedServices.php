e12f4977b7d36f6e71e5721f2c6777ccdd3caa4b
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-12 18:07:57 +0300

    added error handling for getFollowingsPosts

--- backend/app/Services/PostServices/FeedServices.php
+++ backend/app/Services/PostServices/FeedServices.php
@@ -10,8 +10,14 @@ class FeedServices {
     public function getFollowingsPosts($request)
     {
         $followingIds = auth()->user()->followings()->where('status', 'accepted')->pluck('followed_id');
-
+        if(!$followingIds){
+            throw new \Exception('No followings found.',500);
+        }
         $posts = Post::with(['user:id,first_name,last_name'])->whereIn('user_id', $followingIds)->latest()->paginate(10);
+        
+        if (!$posts) {
+            throw new \Exception('No posts found for followings.', 404);
+        }
         
         return $posts;
     }
