ecf92ca60108b4a9f1ca4e7eb43e86b1e9ccaa99
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-14 00:38:39 +0300

    fixed factory typo

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -59,7 +59,7 @@ class UserFactory extends Factory
 
             'fitness_level' => fake()->numberBetween(1,3),
             'fitness_goal' => fake()->randomElement(['weight loss', 'muscle gain', 'endurance', 'flexibility', 'general fitness']),
-            'fitness_interests' => json_encode([fake()->randomElement(['none','home','gym'])]),
+            'fitness_interests' => json_encode([fake()->randomElement(['strength', 'cardio', 'flexibility', 'balance', 'endurance'])]),
             'injuries' => fake()->randomElement(['arm', 'shoulder', 'chest', 'back', 'leg']),
             'medical_conditions' => fake()->words(2, true),
             'allergies' => fake()->optional()->words(2, true),
