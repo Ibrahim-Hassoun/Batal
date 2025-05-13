30ab18faed733350ac5ea586ccac09d339844699
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-14 02:39:13 +0300

    initialized leaderboard controller

---
+++ backend/app/Http/Controllers/LeaderBoardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class LeaderBoardController extends Controller
+{
+    use HttpResponseTrait;
+
+    
+
+    public function __construct()
+    {
+        
+    }
+}
