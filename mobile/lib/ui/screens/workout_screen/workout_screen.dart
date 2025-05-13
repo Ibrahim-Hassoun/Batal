9ee66073d4a76c78fe15cf0bfce9bd69808321f7
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-14 02:13:32 +0300

    added dynamic trophies count

--- mobile/lib/ui/screens/workout_screen/workout_screen.dart
+++ mobile/lib/ui/screens/workout_screen/workout_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/core/provider/AuthProvider.dart';
 import 'package:mobile/core/provider/workout_provider.dart';
 import 'package:provider/provider.dart';
 import '../../widgets/workout/appbar/workout_appbar.dart';
@@ -15,9 +16,12 @@ class _WorkoutScreenState extends State<WorkoutScreen>{
   
   @override
   Widget build(BuildContext context) {
-    final Widget CurrentSection = Provider.of<WorkoutProvider>(context, listen: true).provideSection();
+    WorkoutProvider workoutProvider = Provider.of<WorkoutProvider>(context, listen: true);
+    final Widget CurrentSection = workoutProvider.provideSection();
+
+    AuthProvider authProvider = Provider.of<AuthProvider>(context, listen: true);
     return Scaffold(
-      appBar: WorkoutAppbar(trophies: 0, streak: 0, coins: 0, sets: 0),
+      appBar: WorkoutAppbar(trophies: authProvider.user!['trophies_count'], streak: 0, coins: 0, sets: 0),
       
       body: Column(
         children: [
