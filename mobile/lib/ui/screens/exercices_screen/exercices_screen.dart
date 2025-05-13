16e1c8d266dbb13b33f7da7e012c83d98b212000
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-13 20:33:26 +0300

    added exercices filters

--- mobile/lib/ui/screens/exercices_screen/exercices_screen.dart
+++ mobile/lib/ui/screens/exercices_screen/exercices_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/core/provider/workout_provider.dart';
 import 'package:mobile/main.dart';
+import 'package:provider/provider.dart';
 import '../../widgets/exercices/exercice_row_in_screen.dart';
+import '../../../ui/layouts/drop_down_button.dart';
 
 class ExercicesScreen extends StatefulWidget{
 
@@ -12,11 +15,13 @@ class ExercicesScreen extends StatefulWidget{
 
 class _ExercicesScreenState extends State<ExercicesScreen> {
 
-  
+
 
 
   @override
   Widget build(BuildContext context) {
+    WorkoutProvider provider = Provider.of<WorkoutProvider>(context,listen: true);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: tertiaryColor,
@@ -32,9 +37,18 @@ class _ExercicesScreenState extends State<ExercicesScreen> {
         title: Text('Exercices')
       ),
       body: 
-      Stack(
+      Column(
         children: [
           // Scrollable content
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children:[ 
+              CustomDropdownButton(items: ['option 1','option 2'],onChanged: (value){}, label: 'Area',selectedValue: provider.detected_area,),
+              CustomDropdownButton(items: ['option 1','option 2'],onChanged: (value){}, label: 'Area',selectedValue: provider.detected_area,),
+              CustomDropdownButton(items: ['option 1','option 2'],onChanged: (value){}, label: 'Area',selectedValue: provider.detected_area,)
+              ],
+          ),
+          
           SizedBox(
             child: Positioned.fill(
               child: SingleChildScrollView(
@@ -42,7 +56,7 @@ class _ExercicesScreenState extends State<ExercicesScreen> {
                 padding: EdgeInsets.only(bottom: 100), 
                 child: Column(
                   children: [
-                    SizedBox(height: 20),
+                    SizedBox(height: 16),
                     ExerciseRowInScreen(title: 'title', imageUrl: 'https://randomuser.me/api/portraits/men/1.jpg'),
                     SizedBox(height: 8),
                     ExerciseRowInScreen(title: 'title', imageUrl: 'https://randomuser.me/api/portraits/men/1.jpg'),
