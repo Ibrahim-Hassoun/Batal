b1a60d251e5c3aa96ab408ae26c1aafa3c010b60
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-14 03:11:11 +0300

    imported rows

--- mobile/lib/ui/widgets/workout/leaderboard/leaderboard.dart
+++ mobile/lib/ui/widgets/workout/leaderboard/leaderboard.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/ui/widgets/workout/leaderboard/leaderboard_logic.dart';
 import './player_row.dart';
+import './leaderboard.dart';
 
-class Leaderboard extends StatelessWidget {
-  Leaderboard({super.key});
-  late final List<Map<String,dynamic>> players;
- 
+class Leaderboard extends StatefulWidget {
+  const Leaderboard({super.key});
+
+  @override
+  State<Leaderboard> createState() => _LeaderboardState();
+}
+
+class _LeaderboardState extends State<Leaderboard> {
+  late final List<Map<String, dynamic>> players;
+
+  @override
+  void initState() {
+    
+    super.initState();
+    loadRows();
+  }
+  void loadRows()async{
+    players = await fetchRows();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 16,),
-        PlayerRow(playerName: 'John Doe', playerScore: 100, rank: 2,imageUrl: 'https://randomuser.me/api/portraits/men/1.jpg',streak: 40,)
+        SizedBox(height: 16),
+        PlayerRow(
+          playerName: 'John Doe',
+          playerScore: 100,
+          rank: 2,
+          imageUrl: 'https://randomuser.me/api/portraits/men/1.jpg',
+          streak: 40,
+        ),
       ],
     );
   }
