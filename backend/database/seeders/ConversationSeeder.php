5b8cba633cc0f114d96738c0634b292e75b63d97
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-17 19:22:51 +0300

    implemented conversation seeder

--- backend/database/seeders/ConversationSeeder.php
+++ backend/database/seeders/ConversationSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Conversation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ConversationSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class ConversationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Conversation::factory()->count(10)->create();
     }
 }
