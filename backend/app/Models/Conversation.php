2cefb84061ff455e5fd7724feda740d99b131be3
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-17 19:26:24 +0300

    implemented conversation model relationships

--- backend/app/Models/Conversation.php
+++ backend/app/Models/Conversation.php
@@ -9,4 +9,25 @@ class Conversation extends Model
 {
     /** @use HasFactory<\Database\Factories\ConversationFactory> */
     use HasFactory;
+
+        public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function latestMessage()
+    {
+        return $this->hasOne(Message::class)->latestOfMany();
+    }
+
+    public function userOne()
+    {
+        return $this->belongsTo(User::class, 'user_one_id');
+    }
+
+    public function userTwo()
+    {
+        return $this->belongsTo(User::class, 'user_two_id');
+    }
+
 }
