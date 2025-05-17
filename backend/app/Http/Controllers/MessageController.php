73e35dbf4d85cc3a6cfb2c442ed91bfc10de819f
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-17 22:05:48 +0300

    setup message controller

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits\HttpResponseTrait;
+use App\Services\ChatServices\MessageServices;
+
 
 class MessageController extends Controller
 {
-    //
+    use HttpResponseTrait;
+    protected $messageServices;
+
+    public function __construct(MessageServices $messageServices)
+    {
+        $this->messageServices = $messageServices;
+    }
+
+    public function getConversationMessages(Request $request)
+    {
+        
+    }
 }
