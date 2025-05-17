f92a4f1a5278f4e8ab312c2e2cccb69875f24816
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-17 20:41:29 +0300

    implemented get user conversations controller

--- backend/app/Http/Controllers/ConversationController.php
+++ backend/app/Http/Controllers/ConversationController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ConversationController extends Controller
 {
-    //
+    public function getUserConversations(Request $request){
+        
+        try{
+            $post = $this->conversationServices->getUserConversations($request);
+            return $this->respond(true,'Convos retrieved successfully',$post,201);
+
+        }catch(\Exception $e){
+            return $this->respond(false,$e->getMessage(),null,500);
+        }
+    
+    }
 }
