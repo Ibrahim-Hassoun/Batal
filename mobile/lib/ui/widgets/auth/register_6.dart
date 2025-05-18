62870c8e282d1acc30baab5d0634933e152cf4c9
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-19 02:38:18 +0300

    fixed register 6

--- mobile/lib/ui/widgets/auth/register_6.dart
+++ mobile/lib/ui/widgets/auth/register_6.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/core/provider/AuthProvider.dart';
 import 'package:mobile/main.dart';
 import 'package:mobile/ui/atoms/screen_wide_elevated_button.dart';
+import 'package:provider/provider.dart';
 import '../../layouts/labeled_input.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter/gestures.dart';
 
 
 class Register6 extends StatelessWidget {
-  final Function incrementStep;
-  final Function decrementStep;
-  final Function setStep;
-  final Function handleChange;
-  final Map data;
 
-  const Register6({super.key,required this.incrementStep,required this.decrementStep,required this.setStep,required this.data,required this.handleChange});
+  const Register6({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+
+    AuthProvider authProvider= Provider.of<AuthProvider>(context,listen: true);
+    final Function incrementStep=authProvider.incrementStep;
+    final Function decrementStep=authProvider.decrementStep;
+    final Function handleChange=authProvider.handleChange;
+    final Function setStep=authProvider.setStep;
+    final Map data = authProvider.data;
+
     return Expanded(
       child: Column(
         children: [
