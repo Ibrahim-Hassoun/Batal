4e69e9aa9e7dec482cf0640b620c4045722c9e39
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-20 22:19:59 +0300

    setup tailwind

---
+++ react/src/ui/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="bg-red-500 text-white p-4">
+      Hello Tailwind!
+    </div>
+    </>
+  )
+}
+
+export default App
