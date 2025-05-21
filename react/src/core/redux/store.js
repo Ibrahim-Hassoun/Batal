92b03c9a8230c38ea221ab25b7d5084818e904c7
Ibrahim Hassoun <ihassoun73@gmail.com>
2025-05-21 06:37:10 +0300

    connected tiles to backend

--- react/src/core/redux/store.js
+++ react/src/core/redux/store.js
@@ -1,11 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {createLogger} from 'redux-logger';
-
+import StatisticsSlice from './Statistics/slice';
 
 const logger = createLogger();
 const store = configureStore({
     reducer:{
-        
+        Statistics:StatisticsSlice.reducer
     },
     middleware:(defaltMiddleware) => {
         return defaltMiddleware().concat(logger);
--- react/src/ui/features/statistics/pages/Overview.jsx
+++ react/src/ui/features/statistics/pages/Overview.jsx
@@ -1,28 +1,82 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import InfoTiles from '../components/InfoTiles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faBowlFood,  faDumbbell, faPersonChalkboard, faUser } from '@fortawesome/free-solid-svg-icons';
 import TableRow from '../components/TableRow';
 import BasicBarChart from '../components/BarChart';
-import { LineChart, PieChart } from 'recharts';
 import CustomLineChart from '../components/CustomLineChart';
 import CustomPieChart from '../components/PieChart';
-
+import { useDispatch, useSelector } from 'react-redux';
+import StatisticsSlice, { setLoading, storeDietitiansCount, storeUsersCount,storeTrainersCount ,storeExercicesCount} from '../../../../core/redux/Statistics/slice';
+import request from '../../../../lib/remote/axios';
+import requestMethods from '../../../../lib/enums/request.methods'
 
 const Overview = () => {
+
+
+    const dispatch = useDispatch()
+    const StatisticsState = useSelector((global)=>global.Statistics)
+    useEffect(()=>{
+       
+        fetchUsersCount();
+        fetchDietitiansAndTrainers();
+        fetchExercices()
+        
+    },[])
+
+    const fetchUsersCount = async()=>{
+        const response = await request({
+        method: requestMethods.GET,
+        route:'/api/v0.1/statistics/users' 
+        });
+        if(response.success){
+        dispatch(storeUsersCount({totalUsers:response.data}))
+        }
+    }
+    const fetchDietitiansAndTrainers = async()=>{
+        const response = await request({
+        method: requestMethods.GET,
+        route:'/api/v0.1/statistics/users/by-role' 
+        });
+        if(response.success){
+        dispatch(storeDietitiansCount({dietitians:response.data.dietitians}))
+        dispatch(storeTrainersCount({trainers:response.data.trainers}))
+        // dispatch(storeExercicesCount({exercices:response.data}))
+        }
+    }
+    const fetchExercices = async()=>{
+        const response = await request({
+        method: requestMethods.GET,
+        route:'/api/v0.1/statistics/user-exercices' 
+        });
+        console.log(response)
+        if(response.success){
+        
+        dispatch(storeExercicesCount({exercices:response.data}))
+        dispatch(setLoading({loading:false}))
+        }
+    }
+    
+    const totalUsers = StatisticsState.totalUsers
+    const totalTrainers = StatisticsState.trainers
+    const totalDietitians = StatisticsState.dietitians
+    const totalExercices = StatisticsState.exercices
+        
+
+
   return (
     <div className=' w-3/4 h-[90%] m-auto flex flex-col gap-6 '>
         {/* //split horizontal */}
         <div className=' flex-1 flex gap-3'>
             <div className=' w-1/3 flex flex-col gap-3'>
                 <div id='upper-tiles' className=' flex-1 flex gap-2'>
-                    <InfoTiles icon={faCoffee} title='user' count={500}/>
-                    <InfoTiles icon={faCoffee} title='user' count={500}/>
+                    <InfoTiles icon={faUser} title='Total users' count={totalUsers}/>
+                    <InfoTiles icon={faBowlFood} title='Dietitians' count={totalDietitians}/>
                     
                 </div>
                 <div id='lower-tiles ' className=' flex-1 flex gap-2'>
-                    <InfoTiles icon={faCoffee} title='user' count={500}/>
-                    <InfoTiles icon={faCoffee} title='user' count={500}/>
+                    <InfoTiles icon={faPersonChalkboard} title='Trainers' count={totalTrainers}/>
+                    <InfoTiles icon={faDumbbell} title='user' count={totalExercices}/>
                 </div>
             </div>
             <div className='bg-white w-2/3 flex flex-col items-center justify-between'>
